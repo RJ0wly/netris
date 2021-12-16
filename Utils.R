33c3bc77ecd5273c0d5f142859ce7c53b37fa506
RJelin <56268620+RJ0wly@users.noreply.github.com>
2021-12-16 14:39:54 +0100

    Create Utils.R

---
+++ Utils.R
@@ -0,0 +1,249 @@
+convert_gene_id_to_ensembl_from_symbol <- function(data,gene_list){
+  logical_biological_id_ENSEMBL <- FALSE
+  logical_biological_id_SYMBOL <- TRUE
+  is_in_gene_list <- FALSE
+  # check type of entry of genes
+  if(grepl("ENSG",colnames(data)[1])){
+    logical_biological_id_ENSEMBL <- TRUE
+    logical_biological_id_SYMBOL <- FALSE
+    logical_convert_id_ENSEMBL <- FALSE
+    # check the entry of ENSEMBL ID
+    if(logical_biological_id_ENSEMBL){
+      if(grep(".",colnames(data)[1])){
+        logical_convert_id_ENSEMBL <- TRUE
+      }
+    }
+    # change ENSEMBL ID format to get rid of "." in it
+    if(logical_convert_id_ENSEMBL){
+      ensg_point_look_up <- gsub("\\.[0-9]*$", "", colnames(data))
+    }
+    # converting ENSEMBL ID into SYMBOL return a dataframe containing SYMBOL, 
+    # ENTREZID and ENSEMBL
+    if(logical_biological_id_ENSEMBL){
+      convert_ENSEMBL_id<- clusterProfiler::bitr(ensg_point_look_up, 
+                                                 fromType = "ENSEMBL", 
+                                                 toType = c("ENTREZID",
+                                                            "SYMBOL",
+                                                            "ENSEMBL"), 
+                                                 OrgDb = "org.Hs.eg.db")
+    }
+    # renaming column with ENSEMBL ID without "." in it
+    colnames(data) <- ensg_point_look_up
+    # select only ENSEMBL ID that were convert
+    data <- data[,convert_ENSEMBL_id$ENSEMBL]
+    # renaming column with SYMBOL
+    colnames(data) <- convert_ENSEMBL_id$SYMBOL
+    cat("converting ENSEMBL ID to SYMBOl, done\n")
+    
+  }else{
+    cat("genes IDs is already in the good format\n")
+  }
+  return(data)
+}
+
+case_when_character_type <-function(var_of_interest,
+                               min=0,
+                               max=1,
+                               status=c("neg","pos","int")){
+  if(length(status) > 3){
+    stop("status: length vector is not equal to 3 (superior)")
+  }else if(length(status) < 3){
+    stop("status: length vector is not equal to 3 (inferior)")
+  }else{
+    case_when(var_of_interest < min ~ status[1],
+              var_of_interest > max ~ status[2],
+              var_of_interest > min & var_of_interest < max ~ status[3])
+  }
+}
+
+TCGA_DEG_analysis <- function(name_cancer_vec){
+  file_name_path <- "/home/rj/Desktop/TCGA_project/Project_TCGA_signature/scripts/20530_genes_names.txt"
+  genes_name_list <- read.table(file_name_path)
+  file_path <- paste0(paste0(paste0("/home/rj/Desktop/TCGA_project/Project_TCGA_signature/datasets/",
+                                     "TCGA_"),
+                             as.character(name_cancer_vec)),
+                                      ".RDS")
+  cat(paste0(as.character(name_cancer_vec),"\n"))
+  data <- readRDS(file_path)
+  data <- as.data.frame(t(data))
+  data <- convert_gene_id_to_ensembl_from_symbol(data)
+  sample_ID <- rownames(data)
+  data <- as.data.frame(apply(data,2,as.numeric))
+  rownames(data) <- sample_ID
+  col_genes_names <- colnames(data)
+  
+  score_NTN1 <- read.table("/home/rj/Desktop/TCGA_project/Project_TCGA_signature/scripts/enhancer_promoter_NTN1.txt")
+  df_meta_data <- data %>% dplyr::select(intersect(score_NTN1$V1,genes_name_list$V1))
+  
+  
+  compute_zscore <- sapply(df_meta_data, function(df) (df-mean(df))/sd(df))
+  
+  mean_zscore_by_patient <- apply(compute_zscore,1,mean)
+  
+  df_meta_data <- cbind(df_meta_data,"zscore"=mean_zscore_by_patient)
+  
+  # density plot
+  p_density <- ggplot2::ggplot(df_meta_data,aes(x=NTN1)) + 
+    ggplot2::geom_histogram(aes(y=..density..), colour="black", fill="white") +
+    ggplot2::geom_density(alpha=.2, fill="#FF6666")
+  
+  df_meta_data <- df_meta_data %>% mutate(netrin_status = case_when_character_type(
+    var_of_interest = zscore,
+    min = -0.3,
+    max = 0.3))
+  
+  # binding status with expression matrix
+  df <- cbind(data,"status"=df_meta_data$netrin_status)
+  
+  df_tmp <- subset(df, status == "pos" | status == "neg")
+  status <- df_tmp$status
+  
+  intersect_genes_list <- intersect(colnames(data),genes_name_list$V1)
+  # high counts variables
+  index <- colSums(df_tmp[,intersect_genes_list])
+  filter <- index > 20
+  RNA_data_h <- df_tmp[,intersect_genes_list][,filter]
+  
+  #low counts variables
+  index <- colSums(df_tmp[,intersect_genes_list])
+  filter <- index < 20
+  RNA_data_l <- df_tmp[,intersect_genes_list][,filter]
+  
+  # drop na
+  
+  RNA_data_h <- RNA_data_h %>% drop_na()
+  
+  # most variable column
+  var.all <- RNA_data_h %>% apply(2,var)%>% sort(decreasing=T)
+  var.all <- var.all %>% as.data.frame()
+  rowOfvarAll <- row.names(var.all)
+  mostvariable <- rowOfvarAll[1:10000]
+  
+  RNA_data_h_10K <- RNA_data_h[,mostvariable]
+  
+  rec <- recipes::recipe( ~ ., data = RNA_data_h_10K)
+  
+  pca_trans <- rec %>%
+    recipes::step_pca(all_numeric(),num_comp = 10)
+  
+  pca_estimates <- recipes::prep(pca_trans, training = RNA_data_h_10K)
+  pca_data <- bake(pca_estimates, RNA_data_h_10K)
+  
+  p_pca1 <-ggplot(pca_data,aes(x=PC01,y=PC02,colour=status)) + geom_point(size=3)
+  p_pca1 <- p_pca1 + theme(legend.position="top") + ggtitle("10K most variables genes")
+  
+  # DEG
+  status_df <- as.data.frame(status)
+  meta_data <- cbind(rownames(RNA_data_h),status_df)
+  
+  mat <- t(as.matrix(RNA_data_h))
+  meta_data$status <- factor(meta_data$status)
+  
+  dds <- DESeqDataSetFromMatrix(countData=round(mat),colData=meta_data,design= ~ status)
+  dds <- estimateSizeFactors(dds)		
+  dds <- DESeq(dds)
+  res <- results(dds)
+  resDF = as.data.frame(res)
+  resDF = cbind(resDF,"SYMBOL_gene"=rownames(resDF))
+  keep <- resDF[abs(resDF$log2FoldChange) > 1 & resDF$padj < 0.05 & resDF$baseMean > 1,]
+  resDF$genelabels <- factor(resDF$SYMBOL_gene, levels = rownames(keep))
+  
+  # vulcano plot
+  p_vulcano <- ggplot2::ggplot(data=resDF,
+                               aes(x=log2FoldChange,
+                                   y=-log10(padj),
+                                   label = genelabels )) +
+    ggplot2::geom_point(alpha=0.2,
+                        colour="dodgerblue2",
+                        size=3) +
+    ggrepel::geom_text_repel(col = "black", 
+                             na.rm = TRUE, 
+                             box.padding = unit(0.45, "lines"), 
+                             hjust = 1,
+                             force = 10,
+                             force_pull = 10,
+                             max.overlaps = 100) +
+    ggplot2::geom_hline(yintercept = 1.3,linetype = "dashed")+
+    ggplot2::geom_vline(xintercept = 1,linetype = "dashed") +
+    ggplot2::geom_vline(xintercept = -1,linetype = "dashed") +
+    theme_bw()
+  
+  
+  rec <- recipes::recipe( ~ ., data = RNA_data_h[,rownames(keep)])
+  
+  pca_trans <- rec %>%
+    recipes::step_pca(all_numeric(),num_comp = 10)
+  
+  pca_estimates <- recipes::prep(pca_trans, training = RNA_data_h[,rownames(keep)])
+  pca_data <- bake(pca_estimates, RNA_data_h[,rownames(keep)])
+  
+  if(length(rownames(keep)) < 10){
+    p_pca2 <- ggplot(pca_data,aes(x=PC1,y=PC2,colour=status)) + geom_point(size=3)
+    p_pca2 <- p_pca2 + theme(legend.position="top") + ggtitle("most differentially expressed genes")
+  }else{
+    p_pca2 <- ggplot(pca_data,aes(x=PC01,y=PC02,colour=status)) + geom_point(size=3)
+    p_pca2 <- p_pca2 + theme(legend.position="top") + ggtitle("most differentially expressed genes")  
+  }
+  
+  # Survival analysis part
+  
+  file_path_os <- paste0(paste0(paste0("/home/rj/Desktop/TCGA_project/Project_TCGA_signature/OS_datasets/",
+                                                   "OS_"),
+                             as.character(name_cancer_vec)),
+                                                    ".txt")
+  OS_data <- read.csv(file_path_os,sep="")
+  OS_data$sample <- stringr::str_replace_all(OS_data$sample,"-",".")
+  OS_data$sample <- OS_data$sample %>% substring(1,15)
+  intersect_sample <- intersect(sample_ID,OS_data$sample)
+  
+  df_meta_data <- cbind(df_meta_data,"sample"=sample_ID)
+  
+  df_surv <- merge(df_meta_data,OS_data,by="sample")
+  
+  fit <- survminer::surv_fit(Surv(OS.time,OS) ~ + netrin_status, data = df_surv)
+  
+  p_surv <- ggsurvplot(fit,
+                       pval = TRUE, conf.int = TRUE,
+                       risk.table = TRUE, # Add risk table
+                       risk.table.col = "strata", # Change risk table color by groups
+                       linetype = "strata", # Change line type by groups
+                       surv.median.line = "hv", # Specify median survival
+                       ggtheme = theme_bw(), # Change ggplot2 theme
+                       fontsize = 13,
+                       pval.size = 10,
+                       censor.size = 5)
+  
+  df_surv2 <- df_surv[df_surv$netrin_status == "pos" | df_surv$netrin_status == "neg",]
+  
+  fit2 <- survminer::surv_fit(Surv(OS.time,OS) ~ + netrin_status, data = df_surv2)
+  
+  p_surv2 <- ggsurvplot(fit2,
+                             pval = TRUE, conf.int = TRUE,
+                             risk.table = TRUE, # Add risk table
+                             risk.table.col = "strata", # Change risk table color by groups
+                             linetype = "strata", # Change line type by groups
+                             surv.median.line = "hv", # Specify median survival
+                             ggtheme = theme_bw(), # Change ggplot2 theme
+                             fontsize = 13,
+                             pval.size = 10,
+                             censor.size = 5)
+  
+  main_p <- ggarrange(p_pca1,
+                      p_pca2,
+                      p_density,
+                      p_vulcano,
+                      ncol = 2, nrow = 2)
+  
+  splots <-list()
+  splots[[1]] <- p_surv
+  splots[[2]] <- p_surv2
+  
+  survival_plot <- arrange_ggsurvplots(splots,
+                                       print = FALSE,
+                                       ncol = 2, 
+                                       nrow = 1,
+                                       isk.table.height = 0.4)
+  
+  obj <- list("plot"=main_p,"result"=keep,"survplot"=survival_plot)
+  return(obj)
+}
