c1099c0e9247d1b960b53390740d087c37529598
RJelin <56268620+RJ0wly@users.noreply.github.com>
2022-05-18 14:01:40 +0200

    Add files via upload

--- multiple_visium.R
+++ multiple_visium.R
@@ -1,13 +1,25 @@
-library(Seurat)
-library(SeuratData)
-library(ggplot2)
-library(patchwork)
-library(tidyverse)
-library(ggpubr)
-library(rstatix)
-library(RColorBrewer)
-main_path <- getwd()
+base::lapply(c("Seurat",
+               "tidyverse",
+               "SeuratObject",
+               "SeuratData",
+               "patchwork",
+               "tidyverse",
+               "singscore",
+               "escape",
+               "dittoSeq",
+               "ggpubr",
+               "ggrepel",
+               "cowplot",
+               "RColorBrewer",
+               "ggrepel",
+               "harmony",
+               "rstatix",
+               "ggplot2"),
+             require, character.only=T)
 
+set.seed(500)
+
+main_path <- getwd()
 source(paste0(main_path,"/utils.R"))
 
 # create a result folder
@@ -36,13 +48,13 @@ for(i in 1:length(list_patient_filename[["ID_patient"]])){
     tmp_file_name <- paste0(list_patient_filename[["ID_patient"]][i],
                             "_",
                             list_patient_filename[["ID_treatment"]][j])
-
-
+    
+    
     path_list_to_file_patient[[as.character(list_patient_filename[["ID_patient"]][i])]][j] = paste0(main_path,
                                                                                                     "/folder_visium/",
                                                                                                     tmp_file_name,
                                                                                                     "/outs")
-
+    
   }
 }
 
@@ -50,9 +62,9 @@ result_folder_path <- paste0(main_path,"/results","/multiple_slices_results")
 
 # Loop for that allow to create all plot for each visium
 for(i in 1:length(path_list_to_file_patient)){
-
+  
   sub_result_folder_path <- paste0(result_folder_path,"/",list_patient_filename[["ID_patient"]][i])
-
+  
   # check if folder exist if not create it
   if(dir.exists(sub_result_folder_path) == FALSE){
     setwd(result_folder_path)
@@ -61,7 +73,7 @@ for(i in 1:length(path_list_to_file_patient)){
   }else{
     setwd(sub_result_folder_path)
   }
-
+  
   for(j in 1:length(path_list_to_file_patient[i])){
     # load visium experience
     load_visium_1 <- Seurat::Load10X_Spatial(data.dir   = path_list_to_file_patient[[i]][1],
@@ -71,7 +83,7 @@ for(i in 1:length(path_list_to_file_patient)){
     
     load_visium_1@meta.data <- load_visium_1@meta.data %>% 
       mutate(orig.ident = case_when(orig.ident == "SeuratProject" ~ "not treated"))
-
+    
     load_visium_2 <- Seurat::Load10X_Spatial(data.dir   = path_list_to_file_patient[[i]][2],
                                              filename   = "filtered_feature_bc_matrix.h5",
                                              assay      = "Spatial",
@@ -79,11 +91,11 @@ for(i in 1:length(path_list_to_file_patient)){
     
     load_visium_2@meta.data <- load_visium_2@meta.data %>% 
       mutate(orig.ident = case_when(orig.ident == "SeuratProject" ~ "treated"))
-
-
+    
+    
     normalized_data_1 <- SCTransform(load_visium_1, assay = "Spatial", verbose = FALSE)
     normalized_data_2 <- SCTransform(load_visium_2, assay = "Spatial", verbose = FALSE)
-
+    
     patient_merge_data <- merge(normalized_data_1, normalized_data_2)
     # feature plot
     spatial_features_plot <- SpatialFeaturePlot(patient_merge_data,
@@ -98,156 +110,170 @@ for(i in 1:length(path_list_to_file_patient)){
     
     
     VariableFeatures(patient_merge_data) <- c(VariableFeatures(normalized_data_1), VariableFeatures(normalized_data_2))
-    
+    # 
     patient_merge_data <- RunPCA(patient_merge_data,
-                              assay   = "SCT",
-                              verbose = FALSE)
-    patient_merge_data <- FindNeighbors(patient_merge_data,
-                                     reduction = "pca",
-                                     dims      = 1:30)
+                                 assay   = "SCT",
+                                 verbose = FALSE)
+    
+    # Harmony algorithm after Run PCA in order to minimize the batch effect
+    patient_merge_data <- patient_merge_data %>% 
+      RunHarmony("orig.ident", plot_convergence = F,assay.use ="SCT")
+    
+    # merge harmony reduction in the seurat object
+    harmony_embeddings <- Embeddings(patient_merge_data, 'harmony')
+    
+    DimPlot(object    = patient_merge_data, 
+            reduction = "harmony", 
+            pt.size   = 1, 
+            group.by  = "orig.ident")
+    
+    patient_merge_data <- RunUMAP(object    = patient_merge_data, 
+                                  reduction = "harmony",
+                                  dims      = 1:30)
+    patient_merge_data <- FindNeighbors(object    = patient_merge_data,
+                                        reduction = "harmony",
+                                        dims      = 1:30,
+                                        k.param   = 50,
+                                        n.trees   = 100)
+    
     # algorithm 4 is Leiden clustering algorithm (supposed
     # to be better than louvain algorithm)
     patient_merge_data <- FindClusters(patient_merge_data,
-                                    verbose    = FALSE,
-                                    algorithm  = 4,
-                                    resolution = 0.2)
-
-    patient_merge_data <- RunUMAP(patient_merge_data,
-                               reduction = "pca",
-                               dims      = 1:30)
-
+                                       verbose    = FALSE,
+                                       algorithm  = 4,
+                                       resolution = 0.2)
+    
     p1 <- SpatialDimPlot(patient_merge_data,
                          label      = TRUE,
                          label.size = 3)
-    p2 <- DimPlot(patient_merge_data, reduction = "umap", group.by = c("ident", "orig.ident"))
     
-    p3 <- p1 + p2
+    p2 <- DimPlot(patient_merge_data, reduction = "harmony", 
+            group.by = c("ident"))
     
-    pdf("spatial_umap_clustering", height = 16, width = 16)
-    plot(p3)
+    p3 <- DimPlot(patient_merge_data, reduction = "harmony", 
+                  group.by = c("orig.ident"))
+    
+    p4 <- p1 + p2 + p3
+    
+    pdf("spatial_umap_clustering", height = 16, width = 24)
+    plot(p4)
     dev.off()
-
+    
+    gene_to_check <- c("EPCAM","CLDN5","CLDN4","CLDN7","TFF3","DRAXIN")
+    
+    ftr_boxplot <- features_boxplot(patient_merge_data,c(1),gene_to_check)
+    
+    pdf("Features_boxplot_stats.pdf",width = 16,height = 8*ceiling(length(gene_to_check)/2))
+    plot(ftr_boxplot)
+    dev.off()
+    # prepare multiple SCT object, by regressing the minimum median UMI
+    patient_merge_data <- PrepSCTFindMarkers(patient_merge_data,assay = "SCT")
     # Characterizing each cluster with his top 10 most expressed markers
     patient_merge_data_marker <- FindAllMarkers(patient_merge_data,
                                                 test.use = "negbinom",
                                                 min.pct = 0.5,
-                                                logfc.threshold = 0.7)
-
+                                                logfc.threshold = 0.5)
+    
     write.csv(patient_merge_data_marker,file = "markers_per_clusters.csv")
-
+    
+    
     patient_merge_data_marker %>%
       group_by(cluster) %>%
       top_n(n = 10, wt = avg_log2FC) -> top10
-    p_hm <- DoHeatmap(patient_merge_data, features = top10$gene) + NoLegend()  +
-      scale_fill_gradientn(colors = c("brown1", "black", "green"))
-
+    
+    p_hm <- DoHeatmap(patient_merge_data, 
+                      features = top10$gene) + NoLegend()  +
+      scale_fill_gradientn(colors = c("seagreen3", "black", "brown1"))
+    
     pdf("heatmap_top10_marker.pdf",width = 16,height = 12)
     plot(p_hm)
     dev.off()
     
-    if(i == 1){
-      p_epcam <- features_boxplot(vec_clusters = c(2,4),feature = "EPCAM")
-      pdf("boxplot_epcam.pdf",width = 8,height = 7)
-      plot(p_epcam)
-      dev.off()
+    gene.setsfull <- getGeneSets(library = "H")
+    
+    ES_full <- enrichIt(obj = patient_merge_data@assays[["Spatial"]]@counts,
+                        gene.sets = gene.setsfull,
+                        groups = 1000, cores = 4)
+    
+    patient_merge_data <- AddMetaData(patient_merge_data, ES_full)
+    
+    gene_mak <- read.table(paste0(main_path,"/signature_mak_1.csv"),sep="\t",header=F)
+    
+    gene_set_mak <- list("MAK_epithelial"= gene_mak[gene_mak$V2 == "E",]$V1,
+                         "MAK_mesenchymal"= gene_mak[gene_mak$V2 == "M",]$V1)
+    
+    ESmakfull <- enrichIt(obj = patient_merge_data@assays[["Spatial"]]@counts,
+                          gene.sets = gene_set_mak,
+                          groups = 1000, cores = 4 )
+    
+    patient_merge_data <- AddMetaData(patient_merge_data, ESmakfull)
+    
+    # if(i == 1){
+    #   cluster_to_get = 1
+    # }
+    # else{
+    #   cluster_to_get = 1
+    # }
+    sub_metrics <- patient_merge_data@meta.data[patient_merge_data@meta.data$SCT_snn_res.0.2 == 1,]
+    
+    sub_metrics$MAK_score <- sub_metrics$MAK_mesenchymal - sub_metrics$MAK_epithelial
+    
+    if(nrow(sub_metrics[sub_metrics$orig.ident == "not treated",]) <
+       nrow(sub_metrics[sub_metrics$orig.ident == "treated",])){
+      bigger_set <- sub_metrics[sub_metrics$orig.ident == "treated",]
+      lesser_set <- sub_metrics[sub_metrics$orig.ident == "not treated",]
       
-      p_tff3 <- features_boxplot(vec_clusters = c(2,4),feature = "TFF3")
-      pdf("boxplot_tff3.pdf",width = 8,height = 7)
-      plot(p_tff3)
-      dev.off()
-      
-      p_pgr <- features_boxplot(vec_clusters = c(2,4),feature = "PGR")
-      pdf("boxplot_pgr.pdf",width = 8,height = 7)
-      plot(p_pgr)
-      dev.off()
-      
-      
-    }else if(i == 2){
-      p_epcam <- features_boxplot(vec_clusters = c(6,1),feature = "EPCAM")
-      pdf("boxplot_epcam.pdf",width = 8,height = 7)
-      plot(p_epcam)
-      dev.off()
-      
-      p_tff3 <- features_boxplot(vec_clusters = c(6,1),feature = "TFF3")
-      pdf("boxplot_tff3.pdf",width = 8,height = 7)
-      plot(p_tff3)
-      dev.off()
-      
-      p_pgr <- features_boxplot(vec_clusters = c(6,1),feature = "PGR")
-      pdf("boxplot_pgr.pdf",width = 8,height = 7)
-      plot(p_pgr)
-      dev.off()
+    }else{
+      bigger_set <- sub_metrics[sub_metrics$orig.ident == "not treated",]
+      lesser_set <- sub_metrics[sub_metrics$orig.ident == "treated",]
     }
-  }
+    
+    NES_list_name <- colnames(sub_metrics)[grep("^HALLMARK_.*",colnames(sub_metrics))]
+    
+    plot_HMRK <- lapply(NES_list_name,function(iterator){
+      
+      p <- ggplot(sub_metrics,aes_string(x = "orig.ident",
+                                    y = iterator,
+                                    fill = "orig.ident")) +
+        geom_boxplot() +
+        theme_light() +
+        ggtitle(paste0("NES :",iterator))
+      
+      return(p)
+    })
+    
+    splitPlots <- split(plot_HMRK, ceiling(seq_along(plot_HMRK)/2))
+    
+    pdf(file = "HMRK_plot.pdf", width = 16, height = 8)
+    lapply(splitPlots, plotPlots)
+    dev.off()
+    
+    p_HEMT <- ggplot(sub_metrics,aes(x = orig.ident, 
+                                     y = HALLMARK_EPITHELIAL_MESENCHYMAL_TRANSITION,
+                                     fill = orig.ident)) +
+      geom_boxplot() +
+      ylab("NES Hallmark EMT") +
+      xlab("") +
+      scale_fill_discrete("") +
+      theme_light() +
+      ggtitle("HALLMARK EMT")
+    
+    p_MAK <- ggplot(sub_metrics,aes(x = orig.ident, 
+                                    y = MAK_score,
+                                    fill = orig.ident)) +
+      geom_boxplot() +
+      ylab("NES score MAK") +
+      xlab("") +
+      scale_fill_discrete("") +
+      theme_light() +
+      ggtitle("Score MAK")
+    
+    p_NES <- p_HEMT + p_MAK
+    
+    pdf("NES_score_boxplot.pdf",width = 16,height = 8)
+    plot(p_NES)
+    dev.off()
+      
+  }  
   setwd(result_folder_path)
 }
-
-
-# test zone ---------------------------------------------------------------
-
-
-# load_visium_1 <- Seurat::Load10X_Spatial(data.dir   = path_list_to_file_patient[[1]][1],
-#                                          filename   = "filtered_feature_bc_matrix.h5",
-#                                          assay      = "Spatial",
-#                                          slice      = "not treated")
-# 
-# load_visium_1@meta.data <- load_visium_1@meta.data %>% 
-#   mutate(orig.ident = case_when(orig.ident == "SeuratProject" ~ "not treated"))
-# 
-# load_visium_2 <- Seurat::Load10X_Spatial(data.dir   = path_list_to_file_patient[[1]][2],
-#                                          filename   = "filtered_feature_bc_matrix.h5",
-#                                          assay      = "Spatial",
-#                                          slice      = "treated")
-# 
-# load_visium_2@meta.data <- load_visium_2@meta.data %>% 
-#   mutate(orig.ident = case_when(orig.ident == "SeuratProject" ~ "treated"))
-# 
-# 
-# normalized_data_1 <- SCTransform(load_visium_1, assay = "Spatial", verbose = FALSE)
-# normalized_data_2 <- SCTransform(load_visium_2, assay = "Spatial", verbose = FALSE)
-# 
-# patient_merge_data <- merge(normalized_data_1, normalized_data_2)
-# # feature plot
-# spatial_features_plot <- SpatialFeaturePlot(patient_merge_data,
-#                                             features = c("NTN1","UNC5B" ,"EPCAM","PTPRC","PECAM1","ACTA2"),
-#                                             alpha = c(0.05, 1),
-#                                             min.cutoff = 0)
-# pdf("spatial_features_plot_genes.pdf",
-#     height = 20,
-#     width = 16)
-# plot(spatial_features_plot)
-# dev.off()
-# 
-# 
-# VariableFeatures(patient_merge_data) <- c(VariableFeatures(normalized_data_1), VariableFeatures(normalized_data_2))
-# 
-# patient_merge_data <- RunPCA(patient_merge_data,
-#                              assay   = "SCT",
-#                              verbose = FALSE)
-# patient_merge_data <- FindNeighbors(patient_merge_data,
-#                                     reduction = "pca",
-#                                     dims      = 1:30)
-# # algorithm 4 is Leiden clustering algorithm (supposed
-# # to be better than louvain algorithm)
-# patient_merge_data <- FindClusters(patient_merge_data,
-#                                    verbose    = FALSE,
-#                                    algorithm  = 4,
-#                                    resolution = 0.2)
-# 
-# patient_merge_data <- RunUMAP(patient_merge_data,
-#                               reduction = "pca",
-#                               dims      = 1:30)
-# 
-# p1 <- SpatialDimPlot(patient_merge_data,
-#                      label      = TRUE,
-#                      label.size = 3)
-# p2 <- DimPlot(patient_merge_data, reduction = "umap", group.by = c("ident", "orig.ident"))
-# 
-# p3 <- FeaturePlot(patient_merge_data,features=("EPCAM"),reduction="umap",cols = rev(brewer.pal(n = 11, name = "RdYlBu")))
-# 
-# spatial_features_plot <- SpatialFeaturePlot(patient_merge_data,
-#                                             features = c("EPCAM","TFF3"),
-#                                             alpha = c(0.05, 1),
-#                                             min.cutoff = 0)
-# 
-# p4 <- p2 + p3 + p1 + spatial_features_plot + p_epcam
