a3b354ec13c9b418682df521653b0938a2f67e47
RJelin <56268620+RJ0wly@users.noreply.github.com>
2022-06-09 17:41:20 +0200

    Update Visual_interface_visium.R

--- Visual_interface_visium.R
+++ Visual_interface_visium.R
@@ -20,6 +20,8 @@ base::lapply(c("Seurat",
                "scales"),
              require, character.only=T)
 
+source("utils.R")
+
 main_path <- getwd()
 
 # list name of files in folder_visium and store path of this folder into a vector that contains those paths
@@ -52,7 +54,7 @@ ui <- fluidPage(
                     max   = 1,
                     step  = 0.1),
         br()
-        ),
+      ),
       conditionalPanel(
         condition = "input.select_method_clustering == 'Kmeans'",
         sliderInput("cluster_resolution_k_means", "Number of clusters :", 
@@ -60,8 +62,8 @@ ui <- fluidPage(
                     min   = 0, 
                     max   = 10,
                     step  = 1),
-        br(),
-        ),
+        br()
+      ),
       actionButton("cluster_action_button","Compute cluster"),
       br(),
       selectizeInput("name_gene",
@@ -74,10 +76,13 @@ ui <- fluidPage(
                      choices = NULL,
                      label = "Select group of identification : ",
                      selected = NULL,
-                     multiple = FALSE)
+                     multiple = FALSE),
+      br(),
+      radioButtons("feature_compare","Choose clusters to compare :",
+                   choices = c("no data"),
+                   selected = "")
     ),
     mainPanel(
-      #textOutput("obj_text"),
       shinycssloaders::withSpinner(
         ui_element = plotOutput(outputId = "cluster_plot"),
         type       = 8,
@@ -88,13 +93,31 @@ ui <- fluidPage(
         type       = 8,
         size       = 2,
         color      = "grey"),
-      plotOutput("feature_plot"),
+      shinycssloaders::withSpinner(
+        ui_element = plotOutput(outputId = "feature_plot"),
+        type       = 8,
+        size       = 2,
+        color      = "grey"),
       shinycssloaders::withSpinner(
         ui_element = plotOutput(outputId = "violin_plot"),
         type       = 8,
         size       = 2,
+        color      = "grey"),
+      shinycssloaders::withSpinner(
+        ui_element = plotOutput(outputId = "spatial_plot"),
+        type       = 8,
+        size       = 2,
+        color      = "grey"),
+      shinycssloaders::withSpinner(
+        ui_element = plotOutput(outputId = "ridge_plot"),
+        type       = 8,
+        size       = 2,
+        color      = "grey"),
+      shinycssloaders::withSpinner(
+        ui_element = plotOutput(outputId = "bxp_plot"),
+        type       = 8,
+        size       = 2,
         color      = "grey")
-      
     )
   )
 )
@@ -140,23 +163,19 @@ server <- function(input, output, session) {
     updateSelectizeInput(session, "name_gene", choices = rownames(obj_data()@assays[["SCT"]]@counts), server = TRUE)
   })
   
-  
   observeEvent(input$load_obj_data, {
     showNotification("Data are loaded")
   })
   
-  # output$obj_text <- renderPrint({
-  #   obj_data()
-  # })
   
   leiden_function <- function(object,res){
-      data <- object
-      print("Leiden occuring")
-      data <- FindClusters(object     = data,
-                           algorithm  = 4,
-                           resolution = res,
-                           verbose    = F)
-      return(data)
+    data <- object
+    print("Leiden occuring")
+    data <- FindClusters(object     = data,
+                         algorithm  = 4,
+                         resolution = res,
+                         verbose    = F)
+    return(data)
   }
   
   k_means_function <- function(object,k){
@@ -177,12 +196,12 @@ server <- function(input, output, session) {
     )
   })
   
-
+  
   cluster_plot_kmeans <- function(object){
     Idents(object) <- "k_means"
-    SpatialPlot(object        = object,
-                   label      = TRUE,
-                   label.size = 3)
+    SpatialPlot(object     = object,
+                label      = TRUE,
+                label.size = 3)
   }
   
   cluster_plot_leiden <- function(object,res){
@@ -197,7 +216,7 @@ server <- function(input, output, session) {
     switch(input$select_method_clustering,
            Leiden = cluster_plot_leiden(clust_obj(),input$cluster_resolution_leiden),
            Kmeans = cluster_plot_kmeans(clust_obj())
-           )
+    )
   })
   
   output$cluster_plot <- renderPlot({plot_cluster_type()})
@@ -229,16 +248,16 @@ server <- function(input, output, session) {
                    ncol = 2,
                    pt.size = 2)
   })
-
+  
   
   violin_leiden <- function(object,res,gene_input,group_id){
     Idents(object) <- paste0("SCT_snn_res.",
                              as.character(res))
-    my_color_palette <- hue_pal()(length(levels(object@active.ident)))
+    my_color_palette <- hue_pal()(length(levels(object@meta.data[,group_id])))
     VlnPlot(object    = object,
-            cols = my_color_palette,
-            group.by = "orig.ident",
-            split.by = group_id,
+            cols      = my_color_palette,
+            group.by  = "Treatment",
+            split.by  = group_id,
             features  = gene_input,
             pt.size   = 0)
     
@@ -247,16 +266,17 @@ server <- function(input, output, session) {
   violin_k_means <- function(object,gene_input,group_id){
     
     Idents(object) <- "k_means"
-    my_color_palette <- hue_pal()(length(levels(object@active.ident)))
+    my_color_palette <- hue_pal()(length(levels(object@meta.data[,group_id])))
     VlnPlot(object    = object,
             cols = my_color_palette,
-            group.by = "orig.ident",
+            group.by = "Treatment",
             split.by = group_id,
             features  = gene_input,
             pt.size   = 0)
   }
   
   observeEvent(input$cluster_action_button,{
+    req(clust_obj)
     get_names <- names(clust_obj()@meta.data)[sapply(clust_obj()@meta.data, is.factor)]
     filter_names <- get_names %in% c("seurat_clusters","ident_clustering")
     get_names <- get_names[!filter_names]
@@ -267,7 +287,7 @@ server <- function(input, output, session) {
   
   plot_violin_cluster_type <- reactive({
     req(input$name_gene)
-    req(clust_obj())
+    req(clust_obj)
     req(input$violin_groupby)
     
     switch(input$select_method_clustering,
@@ -288,6 +308,114 @@ server <- function(input, output, session) {
     
     plot_violin_cluster_type()
   })
+  
+  
+  spatial_plot_drawing <- function(object,feature){
+    Idents(object) <- feature
+    SpatialDimPlot(object     = object,
+                   label      = TRUE,
+                   label.size = 3)
+  }
+  
+  plot_spatial_draw <- reactive({
+    req(clust_obj)
+    req(input$violin_groupby)
+    
+    spatial_plot_drawing(clust_obj(),input$violin_groupby)
+    
+  })
+  
+  output$spatial_plot <- renderPlot({plot_spatial_draw()})
+  
+  observeEvent(input$violin_groupby,{
+    updateRadioButtons(session,"feature_compare",choices = unique(clust_obj()@meta.data[,input$violin_groupby]))
+  })
+  
+  plot_ridge_drawing <- function(object, gene_input, group_id){
+    my_color_palette <- my_color_palette <- hue_pal()(length(levels(object@meta.data[,group_id])))
+    Idents(object) <- group_id
+    RidgePlot(object   = object,
+              cols     = my_color_palette,
+              group.by = group_id,
+              features = gene_input)
+  }
+  
+  plot_ridge_draw <- reactive({
+    req(input$name_gene)
+    req(clust_obj)
+    req(input$violin_groupby)
+    
+    plot_ridge_drawing(object = clust_obj(),
+                       gene_input = input$name_gene,
+                       group_id = input$violin_groupby)
+    
+  })
+  
+  output$ridge_plot <- renderPlot({plot_ridge_draw()})
+
+  plot_boxplot_drawing <- function(object,vec_clusters,feature){
+    intersect_features <- intersect(rownames(object@assays[["SCT"]]@data[,]),feature)
+    
+    message(paste0(c("Features :",intersect_features),collapse=" "))
+    
+    if(length(feature) < 2){
+      features_cluster <- as.data.frame(object@assays[["SCT"]]@data[intersect_features,
+                                                                    WhichCells(object, 
+                                                                               idents = vec_clusters)])
+    }else{
+      features_cluster <- as.data.frame(object@assays[["SCT"]]@data[intersect_features,
+                                                                    WhichCells(object, 
+                                                                               idents = vec_clusters)])
+      features_cluster <- features_cluster %>% t() %>% as.data.frame()
+    }
+    colnames(features_cluster) <- c(intersect_features)
+    
+    features_cluster$ID <- rownames(features_cluster)
+    
+    object@meta.data$ID <- rownames(object@meta.data)
+    
+    obj_df_stats_test <- merge(object@meta.data,features_cluster,by="ID")
+    
+    list_boxplot <- lapply(intersect_features,function(i){
+      stat_test <- obj_df_stats_test %>%
+        wilcox_test(as.formula(paste(as.character(i), "~", "Treatment"))) %>%
+        add_significance()
+      stat_test <- stat_test %>% add_xy_position(x = "Treatment")
+      
+      bxp <- ggboxplot(data = obj_df_stats_test, 
+                       x    = "Treatment", 
+                       y    = i, 
+                       fill = "Treatment")
+      p <- bxp + 
+        stat_pvalue_manual(stat_test, label = "p.signif") +
+        scale_y_continuous(expand = expansion(mult = c(0.05, 0.1))) +
+        ylab(paste(i," expression")) + 
+        xlab("") +
+        scale_fill_discrete("") +
+        ggtitle(paste(i))
+      
+      return(p)
+    }) 
+    ggpubr::ggarrange(
+      plotlist = list_boxplot,
+      ncol     = 2,
+      nrow     = ceiling(length(intersect_features)/2)
+    )
+  }
+  
+  plot_bxp_draw <- reactive({
+    req(input$name_gene)
+    req(clust_obj)
+    req(input$feature_compare)
+    
+    plot_boxplot_drawing(object = clust_obj(),
+                         vec_clusters = input$feature_compare,
+                         feature = input$name_gene)
+    
+  })
+  
+  output$bxp_plot <- renderPlot({plot_bxp_draw()})
+
 }
 
 shinyApp(ui, server)
